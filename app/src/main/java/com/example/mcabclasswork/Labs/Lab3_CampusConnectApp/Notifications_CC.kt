14ee973f7848fd6ebfa618d27d32491a18618dcf
OCTehckie18 <omkar.chakraborty2001@gmail.com>
2026-02-10 20:40:33 +0530

    Notifications_CC.kt partially fixed

--- app/src/main/java/com/example/mcabclasswork/Labs/Lab3_CampusConnectApp/Notifications_CC.kt
+++ app/src/main/java/com/example/mcabclasswork/Labs/Lab3_CampusConnectApp/Notifications_CC.kt
@@ -1,8 +1,20 @@
 package com.example.mcabclasswork.Labs.Lab3_CampusConnectApp
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun Notifications_CC(){
-    // TODO
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Displaying All Notifications later.")
+    }
 }
